b0c8d7464c9a23da7cc0a40e67e0f112da2ac3e7
Charlie Whiteside <charlie.whiteside@rocksteadymusicschool.com>
2022-11-23 15:47:37 +0000

    add email to user login and signup

--- src/utils/signup.js
+++ src/utils/signup.js
@@ -1,10 +1,11 @@
-export default async function signup(username, password, loginState) {
+export default async function signup(username, password, email, loginState) {
   const res = await fetch("http://localhost:5001/sign-up", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       username: username,
       password: password,
+      email: email,
     }),
   });
 
