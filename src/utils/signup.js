72b6428afe2450aa7974240e713bf94f429d590a
Charlie Whiteside <charlie.whiteside@rocksteadymusicschool.com>
2022-11-23 14:48:46 +0000

    Add navbar component and api utilities

---
+++ src/utils/signup.js
@@ -0,0 +1,17 @@
+export default async function signup(username, password, loginState) {
+  const res = await fetch("http://localhost:5001/sign-up", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  });
+
+  if (res.data.accountCreated) {
+    loginState(true);
+  } else {
+    loginState(false);
+    throw new Error("Login failed");
+  }
+}
