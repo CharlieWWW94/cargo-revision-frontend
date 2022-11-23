cd414c7ae27eefcf7ccd2059674936f5490d9f56
Charlie Whiteside <charlie.whiteside@rocksteadymusicschool.com>
2022-11-23 14:51:16 +0000

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Icon from "@mdi/react";
+import "bulma/css/bulma.min.css";
 import { mdiFerry } from "@mdi/js";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Team Cargo!</h1>
-      <Icon path={mdiFerry} size={2} />
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <h1>Hello Team Cargo!</h1>
+        <Icon path={mdiFerry} size={2} />
+      </main>
     </div>
   );
 }
