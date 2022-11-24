2bcf82bf0b09c40dc05b67c18ba26adc1dbd9c7c
Charlie Whiteside <charlie.whiteside@rocksteadymusicschool.com>
2022-11-24 11:17:01 +0000

    Create user signup/login forms and create state to manage login status and current username

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,3 +1,10 @@
+import RegForm from "../components/RegForm";
+
 export default function LoginPage(props) {
-  return <h1>LogInPage</h1>;
+  return (
+    <>
+      <h1>LogInPage</h1>
+      <RegForm type={"Log in"} login={props.login} />
+    </>
+  );
 }
